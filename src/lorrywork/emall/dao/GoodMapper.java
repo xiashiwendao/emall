4e17f52352ae232c3c2e83001fe6e27ac817a1ec
lorry <xiashiwendao@sina.com>
2016-12-11 16:41:05 +0800

    sync for PC

---
+++ src/lorrywork/emall/dao/GoodMapper.java
@@ -0,0 +1,8 @@
+package lorrywork.emall.dao;
+
+import lorrywork.emall.entity.Product;
+
+public interface GoodMapper {
+	
+	public void insertPood(Product good);
+}
---
+++ src/lorrywork/emall/entity/ConfigInfo.java
@@ -0,0 +1,7 @@
+package lorrywork.emall.entity;
+
+public class ConfigInfo {
+	public String value;
+	private String key;
+	private String comment;
+}
